902db90ae37427dd36f374ba547f22d0c8ec892d
whyozen <whyozen@gmail.com>
2025-04-25 18:05:44 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'Knight',
+  author: 'Bot',
+  botName: "XIN BOT",
+  botOwner: 'Makoto', // Your name
+  ownerNumber: '6285727435389', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
